7d45e49d2fefbd970dfbb0f8929b00da3d019020
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-06-18 17:01:20 +0530

    feat : consumer    added

--- producer.js
+++ producer.js
@@ -1,42 +1,35 @@
-const { Partitioners } = require("kafkajs");
 const { kafka } = require("./client");
 const readline = require("readline");
 
-const read = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 async function init() {
-  const producer = kafka.producer({
-    createPartitioner: Partitioners.DefaultPartitioner,
-  });
+  const producer = kafka.producer();
 
   console.log("Connecting Producer");
-
   await producer.connect();
   console.log("Producer Connected Successfully");
 
-  read.setPrompt("> ");
-  read.prompt();
+  rl.setPrompt("> ");
+  rl.prompt();
 
-  read.on("line", async function (line) {
+  rl.on("line", async function (line) {
     const [riderName, location] = line.split(" ");
-
-    await producer
-      .send({
-        topic: "rider-updates",
-        messages: [
-          {
-            partition: location.toLowerCase() === "north" ? 0 : 1,
-            key: "location-update",
-            value: JSON.stringify({ name: riderName, location: location }),
-          },
-        ],
-      })
-      .on("close", async () => {
-        await producer.disconnect();
-      });
+    await producer.send({
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location.toLowerCase() === "north" ? 0 : 1,
+          key: "location-update",
+          value: JSON.stringify({ name: riderName, location }),
+        },
+      ],
+    });
+  }).on("close", async () => {
+    await producer.disconnect();
   });
 }
 
